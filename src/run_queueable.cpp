68f7d57002df30469145f5ac0e186ce608711b97
Adam Lamar <adamonduty@gmail.com>
2012-04-20 17:40:40 -0400

    Added platform information to tests

--- src/run_queueable.cpp
+++ src/run_queueable.cpp
@@ -1,36 +1,35 @@
 #include <map>
 #include <stdio.h>
 #include <string>
-#include <vector>
+#include <sys/utsname.h>
 
 #include "tcp.h"
 #include "udss.h"
 
-std::map<std::string, std::string> options;
-
-void run_tcp()
-{
-    Tcp tcp;
-    tcp.run_tests(options);
-}
-
-void run_udss()
-{
-    Udss udss;
-    udss.run_tests(options);
-}
-
 int main()
 {
+    struct utsname name;
+    std::map<std::string, std::string> options;
+
     options["port"] = "10000";
     options["items"] = "1000000";
     options["max_msg_size"] = "128";
     options["max_threads"] = "4";
     options["path"] = "/tmp/queueable-socket";
 
+    uname(&name);
+
+    printf("<?xml version=\"1.0\">\n");
     printf("<tests>\n");
-    run_tcp();
-    run_udss();
+    printf("<platform><sysname>%s</sysname><release>%s</release><version>%s</version><machine>%s</machine></platform>\n",
+      name.sysname, name.release, name.version, name.machine);
+
+    Tcp tcp;
+    tcp.run_tests(options);
+
+    Udss udss;
+    udss.run_tests(options);
+
     printf("</tests>\n");
 
     return 0;
